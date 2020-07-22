243812c6343ff03750346a2fdaa69e007c3aae00
emiestevarena <65693639+emiestevarena@users.noreply.github.com>
2020-07-22 10:40:13 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Emi_libraries
 Creating an algorithm library!
-
-algorithm 1: insertionSort
-Languages available: javascript
+Currently in Javascript
+1.- Insertion Sort Algorithm:
+a.- a1 > a2
+b.- a1 < a2 with shift and push methods 
+2.- Number Center Algorithm
--- library1.js
+++ library1.js
@@ -16,3 +16,48 @@ function insertionSort(array){
     return array;
 }
 console.log(insertionSort(array));
+
+// a1<a2 insertion sort w/ method
+function insertionSortB(array){
+    for(j=1;j<array.length;j++){
+        var key= array[j]
+        var i = j-1;
+        while(i>0 && array[i]<key){
+            array[i+1] = array[i];
+            i = i-1;
+            array[i+1] = key;
+        }
+    }
+    if(array[0]<array[array.length-1]){array.push(array[0]); array.shift(array[0])}
+    return array;
+}
+
+
+//2) number center
+function centernumber(j){
+    var	centro=[];
+    var	c=1;
+    var	i=0;
+    do{
+      c += 1;
+      var a=1;
+      var	suma1=0;
+      var suma2=0;
+      while(a<c){
+        suma1 +=a;
+        a +=1;
+      };
+      b=c+1;
+      while(suma2<suma1){
+        suma2 += b;
+        b +=1;
+      };
+      if(suma1===suma2){
+        centro[i]=c;
+        i +=1;
+      }
+    }while(i<j);
+    return centro;
+    }
+    
+    console.log(centernumber(5));
