28fb0f0031f8b9e3e79711956eaf169e57b13f70
Emiliano Estevarena <65693639+emiestevarena@users.noreply.github.com>
2020-09-07 12:54:08 -0300

    Create DoWhileSort.js

---
+++ javascript/DoWhileSort.js
@@ -0,0 +1,19 @@
+var array2 = [3,41,52,26,38,57,9,49,5,2,4,7,1,3,2,6,10];
+
+function DoWhileSort(array){
+  var i=1, j=0, k=0;
+  do{
+    var l = array[i]
+    if(l<array[i-1]){
+      array[i]=array[i-1];
+      array[i-1]=l;
+      j++;i=0;
+    }
+    i++; k++;
+  }while(i<array.length);
+  console.log("Inversiones: "+j);
+  console.log("Comparaciones: "+k);
+  return array;
+}
+
+console.log(DoWhileSort(array2));
