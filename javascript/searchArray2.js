50220e5154a36ae5993a3ac0eb1688c8488b5804
Emiliano Estevarena <65693639+emiestevarena@users.noreply.github.com>
2020-09-08 11:30:10 -0300

    Update searchArray2.js

--- javascript/searchArray2.js
+++ javascript/searchArray2.js
@@ -2,7 +2,7 @@ function SearchArray2(array,value){
     var i=0;
     var j=array.length-1;
     while(i!=j){
-				if(array[i]==value){
+	if(array[i]==value){
           return value+" is at index "+i;
         }else if(array[j]==value){
           return value+" is at index "+j;
